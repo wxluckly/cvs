906ed0f71ca252b243003a0a2539e672392329c6
Joey wang <wxluckly@gmail.com>
2014-09-02 23:26:33 +0800

    coding

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -2,6 +2,7 @@ class NodesController < ApplicationController
 
   def show
     @node = Node.find(params[:id])
+    raise ActiveRecord::RecordNotFound if @node.page_type.blank?
     render @node.page_type || "show"
   end
 
