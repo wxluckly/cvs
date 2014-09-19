dcc2f5f9bd0a6450cf453e6bff2a578f804fd90a
Joey wang <wxluckly@gmail.com>
2014-09-19 11:12:16 +0800

    增加配置

--- config/application.rb
+++ config/application.rb
@@ -14,13 +14,20 @@ module Cvs
 
     config.assets.precompile += %w(origin/css_pbm0l.css origin/css_pgxGU.css origin/css_JD5d6-.css origin/css_UBtNMO.css origin/css_yvjdhf.css new.css reset.css app_bootstrap.css bootstrap-ie6.css ie.css admin.css)
     config.assets.precompile += %w(origin/respond.js origin/js_xAPl0q.js origin/js_n7Lw7P.js origin/js_KAh8fA.js origin/js_b3i2Om.js origin/html5.js origin/js_Dpwj9_.js admin.js )
+    config.assets.precompile += %w( twitter/glyphicons-halflings.png )
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = "Beijing"
+    config.encoding = "utf-8"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.orm :active_record
+    end
+    
   end
 end
