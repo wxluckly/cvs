9e8279eda922c1ae872189a4760368c0dc1b1ce0
Joey wang <wxluckly@gmail.com>
2015-11-13 18:38:14 +0800

    cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,9 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = Proc.new do |source|
+    "http://#{source =~ /(eot|otf|ttf|woff)$/ ? 'chinesevegansociety.com' : 'chinesevegansociety.cdn.smjjl.com'}"
+  end.tap {|p| def p.to_s; end}
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
